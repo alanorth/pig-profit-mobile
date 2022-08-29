aa5d6def2c0210d6c83000f66a03f40a4d5e9c20
Martyn Haigh <MHaigh@abacusbio.co.nz>
2022-08-30 08:57:11 +1200

    Added slider to the static control builder

--- PigTool/PigTool/Helpers/FormattedElementsHelper.cs
+++ PigTool/PigTool/Helpers/FormattedElementsHelper.cs
@@ -190,30 +190,13 @@ namespace PigTool.Helpers
         public static Slider FormSliderInput()
         {
 
-            Label rotationLabel = new Label
-            {
-                Text = "ROTATING TEXT",
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.CenterAndExpand
-            };
-
-            Label displayLabel = new Label
-            {
-                Text = "(uninitialized)",
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.CenterAndExpand
-            };
 
             Slider slider = new Slider
             {
                 Maximum = 360
             };
-            slider.ValueChanged += (sender, args) =>
-            {
-                rotationLabel.Rotation = slider.Value;
-                displayLabel.Text = String.Format("The Slider value is {0}", args.NewValue);
-            };
+
+            return slider;
 
            
         }
