2d796fbbd1b21ae924194c4b6de34543e1f497c6
Martyn H <MHaigh@abacusbio.co.nz>
2022-10-10 12:14:55 +1300

    Added Scaffoled splash screen.  Will need to add custom image to the app.  Changed the name to PigProfit$

---
+++ PigTool/PigTool.Android/SplashActivity.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+using AndroidX.AppCompat.App;
+
+namespace PigTool.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+           
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        // Simulates background work that happens behind the splash screen
+        /* async void SimulateStartup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }*/
+    }
+}
