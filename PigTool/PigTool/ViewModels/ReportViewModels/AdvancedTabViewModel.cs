afc02eb05121bb1d77ac45806a40ec3cf753a52e
Jamie Horrell <102495702+Glyphxy@users.noreply.github.com>
2022-11-11 08:47:46 +1300

    Add dummy graph to advanced tab report page

---
+++ PigTool/PigTool/ViewModels/ReportViewModels/AdvancedTabViewModel.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Linq;
+using System.Windows.Input;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System.ComponentModel;
+
+namespace PigTool.ViewModels.ReportViewModels
+{
+    public class AdvancedTabViewModel : LoggedInViewModel
+    {
+        private PlotModel graphModel { get; set; }
+
+        public PlotModel GraphModel
+        {
+            get
+            {
+                return graphModel;
+            }
+            set
+            {
+                graphModel = value;
+            }
+        }
+
+        public AdvancedTabViewModel()
+        {
+            GraphModel = new PlotModel();
+
+            LoadBarChart();
+        }
+
+        public void LoadBarChart()
+        {
+            GraphModel = null;
+            OnPropertyChanged("GraphModel");
+            var model = new PlotModel {  };
+            model.LegendOrientation = LegendOrientation.Vertical;
+            model.LegendPlacement = LegendPlacement.Outside;
+            model.LegendPosition = LegendPosition.RightMiddle;
+
+            var barSeries = new ColumnSeries
+            {
+                LabelPlacement = LabelPlacement.Inside,
+                LabelFormatString = "{0}",
+                IsStacked = true,
+                Title = "Test 1"
+            };
+
+            barSeries.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(33),
+                Color = OxyColor.Parse("#3498db")
+            });
+
+            barSeries.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(196),
+                Color = OxyColor.Parse("#2ecc71")
+            });
+
+
+            barSeries.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(152),
+                Color = OxyColor.Parse("#9b59b6")
+            });
+
+            barSeries.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(62),
+                Color = OxyColor.Parse("#34495e")
+            });
+
+            barSeries.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(68),
+                Color = OxyColor.Parse("#e74c3c")
+            });
+
+            barSeries.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(101),
+                Color = OxyColor.Parse("#f1c40f")
+            });
+
+            model.Series.Add(barSeries);
+
+
+
+
+            var barSeries2 = new ColumnSeries
+            {
+                LabelPlacement = LabelPlacement.Inside,
+                LabelFormatString = "{0}",
+                IsStacked = true,
+                Title = "Test 2"
+            };
+
+            barSeries2.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(100),
+                Color = OxyColor.Parse("#3498db")
+            });
+
+            barSeries2.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(100),
+                Color = OxyColor.Parse("#2ecc71")
+            });
+
+
+            barSeries2.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(100),
+                Color = OxyColor.Parse("#9b59b6")
+            });
+
+            barSeries2.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(100),
+                Color = OxyColor.Parse("#34495e")
+            });
+
+            barSeries2.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(100),
+                Color = OxyColor.Parse("#e74c3c")
+            });
+
+            barSeries2.Items.Add(new ColumnItem
+            {
+                Value = Convert.ToDouble(100),
+                Color = OxyColor.Parse("#f1c40f")
+            });
+
+
+            model.Series.Add(barSeries2);
+
+
+            String[] strNames = new String[] { "Rabbit", "Dog", "Cat", "Goldfish", "Hamster", "Birds" };
+            model.Axes.Add(new CategoryAxis
+            {
+                Position = AxisPosition.Bottom,
+                Key = "Sample Data",
+                ItemsSource = strNames,
+                IsPanEnabled = false,
+                IsZoomEnabled = false,
+                Selectable = false,
+            });
+
+            GraphModel = model;
+            OnPropertyChanged("GraphModel");
+        }
+
+    }
+}
--- PigTool/PigTool/Views/ReportPages/AdvancedTab.xaml.cs
+++ PigTool/PigTool/Views/ReportPages/AdvancedTab.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Xamarin.Forms;
+using PigTool.ViewModels.ReportViewModels;
+using System;
 using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -7,9 +11,12 @@ namespace PigTool.Views
 {
     public partial class AdvancedTab : ContentPage
     {
+        AdvancedTabViewModel _viewModel;
         public AdvancedTab()
         {
+            _viewModel = new AdvancedTabViewModel();
             InitializeComponent();
+            this.BindingContext = _viewModel;
         }
     }
 }
